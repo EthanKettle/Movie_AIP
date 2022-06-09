5e0326e8bcf51cbcefee2c7261ee2d1ff72b2947
Jonas <jonasmooney2@gmail.com>
2022-06-09 07:45:28 -0600

    Starting day 4, trending api call working

--- movie/src/Trending.js
+++ movie/src/Trending.js
@@ -1,8 +1,25 @@
 import './scss/trending.scss'
 
+
 export default function Trending() {
+  const key = '6b9f844777c1c7871dcdef89004ab36e';
+  let url = `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`;
+  fetch(url)
+  .then(response => response.json())
+  .then(content => {
+    content.results.map((id, title, poster_path, overview, popularity) => {
+      console.log(id);
+    })
+  })
 
   return (
-    <h1>Trending content here</h1>
+    <div className='trending'>
+
+    </div>
   )
 }
+
+
+// api key 6b9f844777c1c7871dcdef89004ab36e
+// example request https://api.themoviedb.org/3/movie/550?api_key=6b9f844777c1c7871dcdef89004ab36e\
+// api Read Access Token eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YjlmODQ0Nzc3YzFjNzg3MWRjZGVmODkwMDRhYjM2ZSIsInN1YiI6IjYyYTBjY2IxZDIxNDdjMTE3ZTY3YzJmOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Mgew4Nf_BORP76TOmS9vnBmLWnwrSuSsXfLHmyUtOGs
