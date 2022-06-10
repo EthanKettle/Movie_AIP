49e75fad05c9d4b3c25c6bbdad2879dce344140e
Jonas <jonasmooney2@gmail.com>
2022-06-10 08:47:23 -0600

    Movies almost rendering

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import Header from './header/Header';
 import BottomNav from './footer/BottomNav';
 import Search from './Search';
@@ -6,7 +7,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Trending from './Trending';
 
 function App() {
-
+  const [searchResultsDisplay, setDisplay] = useState([]);
+  // 1) Variable containing needed data 2) Setting state to that variable 3) Using state to render JSX
+  const getResults = (data) => {
+    const movieInfo = data.results;
+    setDisplay(movieInfo)
+    const movieData = data.results.map(({id, title, backdrop_path, poster_path, popularity, release_date, overview}) => {
+      return <li key={id}>
+                <img className='movieImage' alt={title}>{poster_path}</img>
+                
+            </li>
+    })
+  }
 
   return (
     <div className="App">
@@ -15,7 +27,7 @@ function App() {
             <Routes>
               <Route exact path='/' />
               <Route path='/trending' element={<Trending />} />
-              <Route path='/search' element={<Search />}/>
+              <Route path='/search' element={<Search getResults={getResults}/>}/>
             </Routes>
             <BottomNav />
           </Router>
--- movie/src/Search.js
+++ movie/src/Search.js
@@ -2,9 +2,14 @@ import React, {useState} from 'react';
 import './scss/search.scss';
 const key = '6b9f844777c1c7871dcdef89004ab36e';
 
-
-export default function Search() {
+export default function Search({getResults}) {
   const [query, setQuery] = useState('');
+  async function ApiCall(query) {
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${query}&page=1`;
+    const response = await fetch(url)
+    const data = await response.json()
+    getResults(data)
+  }
   return (
     <>
       <input 
@@ -23,16 +28,6 @@ export default function Search() {
   )
 }
 
-async function ApiCall(query) {
-  let url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${query}&page=1`;
-  const response = await fetch(url)
-  const data = await response.json()
-  // console.log(data.results)
-  data.results.map(({id, title}) => {
-    console.log(id);
-    return id;
-  })
-}
 
 // function makeSearch(searchInput){
 //   let HTML = `Search Results for ${query}`;
