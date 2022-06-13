256343b8cdd375f5c8dc5c21251738affb8b0dbe
Jonas <jonasmooney2@gmail.com>
2022-06-13 07:38:08 -0600

    Search and trending basic functionality

--- movie/src/Search.js
+++ movie/src/Search.js
@@ -11,40 +11,16 @@ export default function Search({getResults}) {
     getResults(data)
   }
   return (
-    <>
+    <div className='searchAndButton'>
       <input 
       placeholder="Search"
       value={query}
       onChange={e => setQuery(e.target.value)}
       />
-      <button onClick={() => {
+      <button className='searchButton' onClick={() => {
         ApiCall(query)
       }}>Search
       </button>
-      <div className='searchResults'>
-
-      </div>
-    </>
+    </div>
   )
 }
-
-
-// function makeSearch(searchInput){
-//   let HTML = `Search Results for ${query}`;
-//   for (let i = 0; i < page.results.length; i++) {
-//       HTML +=
-//     `<div class="CardStyle" 
-//       onClick={() => {
-//       changePage(pick/${page.results[i]})
-//       }}>
-//       <div class="movieImg">
-//         ${page.results[i].poster_path}
-//       </div>
-//       <div class="info">
-//         <p class="rating">${page.results[i].vote_average}</p>
-//         <span class="name">${page.results[i].title}</span>
-//         <p class="date">${page.results[i].release_date}</p>
-//       </div>
-//     </div>`;
-//   }
-// }
