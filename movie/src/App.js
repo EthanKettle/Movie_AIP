0c2287665752823bf9eae06fb0788f933ce332b8
jonas-mooney <96934456+jonas-mooney@users.noreply.github.com>
2022-06-13 08:45:59 -0600

    Update App.js

--- movie/src/App.js
+++ movie/src/App.js
@@ -5,6 +5,7 @@ import Search from './Search';
 import './css/App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Trending from './Trending';
+import Landing from './landing/Landing';
 
 function App() {
   const singleMovieView = (id) => {
@@ -15,9 +16,7 @@ function App() {
   const getResults = (data) => {
     const movieInfo = data.results;
     setDisplay(movieInfo)
-
   }
-  
   const movieData = searchResultsDisplay.map(({id, title, backdrop_path, poster_path, popularity, release_date, overview}) => {
     return <div onClick={singleMovieView(id)} className='movieCard' key={id}>
               <img src={'https://image.tmdb.org/t/p/original' + poster_path} className='movieImage' alt={title}></img>
@@ -28,9 +27,22 @@ function App() {
   const getTrending = (data) => {
     console.log(data)
     const movieInfo = data.results;
-    setTrendingDisplay(movieInfo)
+    setTrendingDisplay(movieInfo);
   }
   const trendingMovieData = trendingDisplay.map(({id, title, backdrop_path, poster_path, popularity, release_date, overview}) => {
+    return <div onClick={singleMovieView(id)} className='movieCard' key={id}>
+            <div className='imageOverlay'></div>
+            <img src={'https://image.tmdb.org/t/p/original' + poster_path} className='movieImage' alt={title}></img>
+          </div>
+  })
+  
+  const [landingMovies, setLandingMovies] = useState([]);
+  const getLandingMovies = (data) => {
+    console.log(data)
+    const movieInfo = data.results;
+    setLandingMovies(movieInfo);
+  }
+  const landingMovieData = landingMovies.map(({id, title, backdrop_path, poster_path, popularity, release_date, overview}) => {
     return <div onClick={singleMovieView(id)} className='movieCard' key={id}>
             <div className='imageOverlay'></div>
             <img src={'https://image.tmdb.org/t/p/original' + poster_path} className='movieImage' alt={title}></img>
@@ -42,10 +54,9 @@ function App() {
     <div className="App">
           <Router>
             <Header />
-
             <div className='searchContainer'>
               <Routes>
-                <Route exact path='/' />
+                <Route exact path='/' element={<><Landing getLandingMovies={getLandingMovies}/><div className='landingMovieContainer'>{landingMovieData}</div></>}/>
                 <Route path='/trending' element={<><Trending getTrending={getTrending}/><div className='trendingContainer'>{trendingMovieData}</div></>} />
                 <Route path='/search' element={<><Search getResults={getResults}/><div className='movieCardContainer'>{movieData}</div></>} />
               </Routes>
@@ -56,8 +67,6 @@ function App() {
   );
 }
 
-
 export default App;
 
 // Adding page for each movie - Click in to each movie.
-// 
